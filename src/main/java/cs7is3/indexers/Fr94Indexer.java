4ec0d8d8c935c0830ab12b174bbeeed366d9e6e2
Sophie Crowley <crowleso@tcd.ie>
2021-11-05 15:47:47 +0000

    Adding extra fields to Fr94 document
    The 'TEXT' field contains all of the text within <TEXT>. The OUTER_TEXT field contains all of the text that is Not inside a nested node. Then, I am getting the text inside the main nested nodes.

--- src/main/java/cs7is3/indexers/Fr94Indexer.java
+++ src/main/java/cs7is3/indexers/Fr94Indexer.java
@@ -1,5 +1,8 @@
 package cs7is3.indexers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
@@ -9,10 +12,32 @@ public class Fr94Indexer
 {
     public static Document fillDocument(Element element, Document document)
     {
+        // Add the DOCNO as an ID.
         String docNumber = element.getElementsByTag("DOCNO").first().text();
         document.add(new TextField("DOCNO", docNumber, Field.Store.YES));
-        String text = element.getElementsByTag("TEXT").first().text().replaceAll("\n+", "\n");
-        document.add(new TextField("TEXT", text, Field.Store.YES));
+
+        // Add all of the text inside <TEXT>, including text inside nested elements.
+        String allText = element.getElementsByTag("TEXT").first().text().replaceAll("\n+", "\n");
+        document.add(new TextField("TEXT", allText, Field.Store.YES));
+
+        // Add the text inside <TEXT> without any of the text inside nested elements.
+        String outerText = element.getElementsByTag("TEXT").first().ownText().replaceAll("\n+", "\n");
+        document.add(new TextField("OUTER_TEXT", outerText, Field.Store.YES));
+
+        List<String> extraFields = Arrays.asList("AGENCY", "ACTION", "SUMMARY", "FURTHER", "SUPPLEM");
+        for (int i = 0; i < extraFields.size(); i++) {
+            addExtraField(extraFields.get(i), element, document);
+        }
+        return document;
+    }
+
+    public static Document addExtraField(String field, Element element, Document document)
+    {
+        // Add the text inside an element of type field, if it exists.
+        Element node = element.getElementsByTag(field).first();
+        if (node != null) {
+            document.add(new TextField(field, node.text().replaceAll("\n+", "\n"), Field.Store.YES));
+        }
         return document;
     }
 }
