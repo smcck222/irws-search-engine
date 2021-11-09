644d0e6eec52e8e95b9ff8e1c2a7418180813609
pengb111 <93585762+pengb111@users.noreply.github.com>
2021-11-09 11:42:28 +0000

    Added LATIndexer

--- src/main/java/cs7is3/indexers/LATIndexer.java
+++ src/main/java/cs7is3/indexers/LATIndexer.java
@@ -11,31 +11,37 @@ import java.util.List;
 
 public class LATIndexer
 {
-       public static Document fillDocument(Element element, Document document)
+      public static Document fillDocument(Element element, Document document)
     {
         // Add the <DOCNO> and avoid the Null value
-        String docNumber = element.getElementsByTag("DOCNO").first().text();
         if (element.getElementsByTag("DOCNO").first() == null)
-            document.add(new StringField("DOCNO", "", Field.Store.YES));
-        document.add(new StringField("DOCNO", docNumber, Field.Store.YES));
+            document.add(new StringField("DOCNO", " ", Field.Store.YES));
+        else {
+            String docNumber = element.getElementsByTag("DOCNO").first().text();
+            document.add(new StringField("DOCNO", docNumber, Field.Store.YES));
+        }
 
         // Add the <HEADLINE> and avoid the Null value
-        String headline = element.getElementsByTag("HEADLINE").first().text();
         if (element.getElementsByTag("HEADLINE").first() == null)
-            document.add(new TextField("HEADLINE", "", Field.Store.YES));
-        document.add(new TextField("HEADLINE", headline, Field.Store.YES));
+            document.add(new TextField("HEADLINE", " ", Field.Store.YES));
+        else {
+            String headline = element.getElementsByTag("HEADLINE").first().text();
+            document.add(new TextField("HEADLINE", headline, Field.Store.YES));
+        }
 
         // Add the <TEXT> and avoid the Null value
-        String text = element.getElementsByTag("TEXT").first().text();
-        if(element.getElementsByTag("TEXT").first().text()=="")
-            document.add(new TextField("TEXT",text,Field.Store.YES));
-        document.add(new TextField("TEXT", text, Field.Store.YES));
-        
-        // Add the extra content
+        if(element.getElementsByTag("TEXT").first() == null)
+            document.add(new TextField("TEXT"," ",Field.Store.YES));
+        else {
+            String text = element.getElementsByTag("TEXT").first().text();
+            document.add(new TextField("TEXT", text, Field.Store.YES));
+        }
+
         List<String> extraFields = Arrays.asList("DOCID","DATE","LENGTH","GRAPHIC","SECTION","SUBJECT","TYPE");
         for (int i = 0; i < extraFields.size(); i++) {
             addExtraField(extraFields.get(i), element, document);
         }
+        
         return document;
     }
 
