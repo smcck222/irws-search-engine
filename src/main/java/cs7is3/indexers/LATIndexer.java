ab5551517f22490d597cc6af9aa95d281f3d2e38
pengb111 <93585762+pengb111@users.noreply.github.com>
2021-11-09 11:29:46 +0000

    Added LATIndexer

--- src/main/java/cs7is3/indexers/LATIndexer.java
+++ src/main/java/cs7is3/indexers/LATIndexer.java
@@ -7,9 +7,39 @@ import org.jsoup.nodes.Element;
 
 public class LATIndexer
 {
-    public static Document fillDocument(Element element, Document document)
+       public static Document fillDocument(Element element, Document document)
     {
-        document.add(new TextField("content", element.text(), Field.Store.YES));
+        // Add the <DOCNO> and avoid the Null value
+        String docNumber = element.getElementsByTag("DOCNO").first().text();
+        if (element.getElementsByTag("DOCNO").first() == null)
+            document.add(new TextField("DOCNO", "", Field.Store.YES));
+        document.add(new TextField("DOCNO", docNumber, Field.Store.YES));
+
+        // Add the <HEADLINE> and avoid the Null value
+        String headline = element.getElementsByTag("HEADLINE").first().text();
+        if (element.getElementsByTag("HEADLINE").first() == null)
+            document.add(new TextField("HEADLINE", "", Field.Store.YES));
+        document.add(new TextField("HEADLINE", headline, Field.Store.YES));
+
+        // Add the <TEXT> and avoid the Null value
+        String text = element.getElementsByTag("TEXT").first().text();
+        if(element.getElementsByTag("TEXT").first().text()=="")
+            document.add(new TextField("TEXT",text,Field.Store.YES));
+        document.add(new TextField("TEXT", text, Field.Store.YES));
+        
+        // Add the extra content
+        List<String> extraFields = Arrays.asList("DOCID","DATE","LENGTH","GRAPHIC","SECTION","SUBJECT","TYPE");
+        for (int i = 0; i < extraFields.size(); i++) {
+            addExtraField(extraFields.get(i), element, document);
+        }
         return document;
+    }
+
+    public static void addExtraField(String field, Element element, Document document) {
+        // Add the text inside an element of type field, if it exists.
+        Element node = element.getElementsByTag(field).first();
+        if (node != null) {
+            document.add(new TextField(field, node.text().replaceAll("\n+", "\n"), Field.Store.YES));
+        }
     }
 }
