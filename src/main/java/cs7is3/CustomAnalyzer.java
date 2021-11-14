ab935ff10c5f90c9c27033fdcfbe5599b0a442d9
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2021-11-14 11:54:37 +0000

    Added dynamic stopword loading and changed to longer list of words

--- src/main/java/cs7is3/CustomAnalyzer.java
+++ src/main/java/cs7is3/CustomAnalyzer.java
@@ -1,6 +1,8 @@
 package cs7is3;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -12,37 +14,33 @@ import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 
-public class CustomAnalyzer extends Analyzer {
-    @Override
-    protected TokenStreamComponents createComponents(String fieldName) {
-        // credit: https://www.ranks.nl/stopwords
-        List<String> stopwords = Arrays.asList(
-            "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
-            "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
-            "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
-            "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during",
-            "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have",
-            "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
-            "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've",
-            "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me",
-            "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on",
-            "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over",
-            "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't",
-            "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them",
-            "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll",
-            "they're", "they've", "this", "those", "through", "to", "too", "under", "until",
-            "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were",
-            "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while",
-            "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you",
-            "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"
-        );
+public class CustomAnalyzer extends Analyzer
+{
+    private static final String STOPWORDS_PATH = "./data/stopwords/en_long.txt";
 
+    private List<String> stopwords;
+
+    public CustomAnalyzer() throws IOException
+    {
+        this.stopwords = loadStopwords();
+    }
+
+    private List<String> loadStopwords() throws IOException
+    {
+        return Files.readAllLines(Paths.get(STOPWORDS_PATH));
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName)
+    {
         StandardTokenizer tokenizer = new StandardTokenizer();
         TokenStream stream;
+
         stream = new LowerCaseFilter(tokenizer);
         stream = new EnglishPossessiveFilter(stream);
-        stream = new StopFilter(stream, new CharArraySet(stopwords, true));
+        stream = new StopFilter(stream, new CharArraySet(this.stopwords, true));
         stream = new PorterStemFilter(stream);
+        
         return new TokenStreamComponents(tokenizer, stream);
     }
 }
