83e4e64b5cb9fd846929c5f1836d13ce79598782
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2021-11-16 07:29:48 +0000

    Add optional second param to run script

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-java -jar target/searchengine-1.0.jar --mode=$1
+if [[ $# -eq 2 ]]
+    then
+        java -jar target/searchengine-1.0.jar --mode=$1 --results=$2
+    else
+        java -jar target/searchengine-1.0.jar --mode=$1
+fi
